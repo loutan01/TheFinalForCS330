1244ff9e2350ce6b7c2feeffb590a3d7d515df2b
loutan01 <loutan01@luther.edu>
2016-05-17 00:28:21 -0500

    dsfd

--- run4meBaby.py
+++ run4meBaby.py
@@ -3,8 +3,7 @@ from flask import Flask
 
 app = Flask(__name__)
 #app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://loutan01:Robbie911@localhost/database'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['WTF_CSRF_SECRET_KEY'] = 'this-should-be-more-random'
 app.secret_key = '140735111582464'
 
@@ -13,4 +12,3 @@ from views import *
 if __name__ == '__main__':
     app.debug = True
     app.run()
-    
