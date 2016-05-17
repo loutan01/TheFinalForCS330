740ac4aedc6361ce0ae0ffb79057a8d76edf28ad
loutan01 <loutan01@luther.edu>
2016-05-17 00:38:58 -0500

    really, this is the last time

--- run4meBaby.py
+++ run4meBaby.py
@@ -3,7 +3,7 @@ import os
 
 app = Flask(__name__)
 #app.config.from_object(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEROKU_POSTGRESQL_WHITE_URL']
 app.config['WTF_CSRF_SECRET_KEY'] = 'this-should-be-more-random'
 app.secret_key = '140735111582464'
 
